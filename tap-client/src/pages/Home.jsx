9a05e7743e693f921c425d8f623ddb520199c5d4
Varun <varun.g.dsu@gmail.com>
2024-08-11 20:56:29 +0530

    copied changes from contibutor

--- tap-client/src/pages/Home.jsx
+++ tap-client/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Tooltip from "../components/ToolTip";
+import Tooltip from "./ToolTip";
 import bubbleImage from "../../public/bubbleremovebgpreview-1-1@2x.png"; // Import the image
 import starIcon from "../../public/iconlysharpboldstar.svg";
 import monitorIcon from "../../public/monitor.svg";
@@ -8,76 +8,62 @@ import lightbulbIcon from "../../public/iconsolidlightbulb.svg";
 
 const Home = ({ togglePopup }) => {
     return (
-        <>
-            
-            <div className="red"></div>
-            <div className="blue"></div>
-            <div className="purple"></div>
+        <div className="body">
+            <div className="bg">
+                <div className="red"></div>
+                <div className="blue"></div>
+                <div className="purple"></div>
+            </div>
             <h1 className="take-a-new" id="main-heading">
                 Take a ‘new turn’
             </h1>
             <div className="we-help-pivot">
-                We help pivot careers with applied learnings and accelerate
-                potential growth and path to excellence.
+                We help pivot careers with applied learnings and <br></br>
+                accelerate potential growth and path to excellence.
             </div>
             <img className="bubble" alt="" src={bubbleImage} />
-            <button
-                id="groupbutton"
-                className="group-button"
-                onClick={togglePopup}
-            >
-                <div className="aprimary-button-wrapper">
-                    <div className="aprimary-button">
-                        <div className="get-started">Get Started</div>
-                    </div>
-                </div>
+            <button id="get-started" onClick={togglePopup}>
+                <div className="get-started">Get Started</div>
             </button>
             <div className="unlock">
-                <span className="unlock-the-talents-container1">
-                    <p className="unlock-the-talents">
-                        Unlock the talents within,
-                    </p>
-                    <p className="learning-for-the">Learning for the future</p>
-                </span>
+                <p className="unlock-the-talents">
+                    Unlock the talents within,<br></br>Learning for the future
+                </p>
             </div>
             <div className="benefits">
-                <div className="benchild">
-                    <div className="quality-education">Quality education</div>
-                    <div className="interactive-engaging">
-                        Interactive & engaging
-                    </div>
-                    <div className="performance-tracking">
-                        Performance tracking
-                    </div>
-                    <div className="compliance-certification">
-                        Compliance & certification
-                    </div>
+                <div className="quality-education">Quality education</div>
+                <div className="interactive-engaging">
+                    Interactive & engaging
                 </div>
-                <img
-                    className="iconlysharpboldstar"
-                    alt=""
-                    src={starIcon}
-                    id="iconlySharpBoldStar"
-                />
-                <img className="monitor-icon" alt="" src={monitorIcon} />
-                <img className="icontrophy" alt="" src={trophyIcon} />
-                <img
-                    className="iconsolidlight-bulb"
-                    alt=""
-                    src={lightbulbIcon}
-                />
-                <div className="line"></div>
-                <div className="line1"></div>
-                <div className="line2"></div>
-                <div className="rectangle-parent">
-                    <div className="group-child"></div>
-                    <div className="member-benefits">Member benefits</div>
+                <div className="performance-tracking">Performance tracking</div>
+                <div className="compliance-certification">
+                    Compliance & certification
                 </div>
+
+                <div className="images">
+                    <img
+                        className="iconlysharpboldstar"
+                        alt=""
+                        src={starIcon}
+                    />
+                    <img className="monitor-icon" alt="" src={monitorIcon} />
+                    <img
+                        className="iconsolidlight-bulb"
+                        alt=""
+                        src={lightbulbIcon}
+                    />
+                    <img className="icontrophy" alt="" src={trophyIcon} />
+                </div>
+                <div className="lines">
+                    <div className="line"></div>
+                    <div className="line1"></div>
+                    <div className="line2"></div>
+                </div>
+                <button className="member-benefits">Member benefits</button>
             </div>
             <div className="line3"></div>
             {/* <Tooltip /> */}
-            
-        </>
+        </div>
     );
 };
 
