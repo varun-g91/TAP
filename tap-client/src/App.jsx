7dfb0283f2f7496623aa21c141eaf4221ac50bf9
Varun <varun.g.dsu@gmail.com>
2024-10-17 19:29:54 +0530

    refined App.js

--- tap-client/src/App.jsx
+++ tap-client/src/App.jsx
@@ -4,7 +4,6 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import VerificationPage from "./pages/VerificationPage";
-import ContactForm from "./components/ContactForm";
 import Modal from "./components/Modal";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -21,6 +20,7 @@ function App() {
         setShowModal(true);
     };
 
+
     const closeModalHandler = () => {
         console.log("Close Modal Handler Called");
         setShowModal(false);
@@ -31,18 +31,29 @@ function App() {
             <div className="desktop-52">
                 <Header />
                 <Routes>
-                    <Route path="/" element={<Home togglePopup={togglePopup} />} />
+                    <Route
+                        path="/"
+                        element={<Home togglePopup={togglePopup} showPopup={showPopup} showModalHandler={showModalHandler} />}
+                    />
                     <Route
                         path="/users/:userId/verify/:token"
                         element={<VerificationPage />}
                     />
                 </Routes>
                 <Footer />
-                {showPopup && (
-                    <ContactForm closePopup={togglePopup} showModal={showModalHandler} />
-                )}
+                {/* {showPopup && (
+                    <div className="contact-form">
+                        <ContactForm
+                            closePopup={togglePopup}
+                            showModal={showModalHandler}
+                        />
+                    </div>
+                )} */}
                 {showModal && (
-                    <Modal message={modalMessage} closeModal={closeModalHandler} />
+                    <Modal
+                        message={modalMessage}
+                        closeModal={closeModalHandler}
+                    />
                 )}
             </div>
         </Router>
